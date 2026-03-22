9fc9030c829c6e1a05ab42d8ab4b3fe8511b5511
Viksata <viki1204394@gmail.com>
2026-03-22 14:42:34 +0200

    Created 4 apps

--- petstagram/settings.py
+++ petstagram/settings.py
@@ -36,6 +36,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'petstagram.photos.apps.PhotosConfig',
+    'petstagram.pets.apps.PetsConfig',
+    'petstagram.common.apps.CommonConfig',
+    'petstagram.accounts.apps.AccountsConfig'
 ]
 
 MIDDLEWARE = [
