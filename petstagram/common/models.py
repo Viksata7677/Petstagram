39b7fe2218f15bd430da8b347a2e6bcd11bd7fb8
Viksata <viki1204394@gmail.com>
2026-03-23 17:12:26 +0200

    Created Like model in common app

--- petstagram/common/models.py
+++ petstagram/common/models.py
@@ -10,3 +10,7 @@ class Comment(models.Model):
     text = models.TextField(max_length=300)
     date_time_of_publication = models.DateTimeField(auto_now_add=True)
     to_photo = models.ForeignKey(to=Photo, on_delete=models.CASCADE)
+
+
+class Like(models.Model):
+    to_photo = models.ForeignKey(to=Photo, on_delete=models.CASCADE)
