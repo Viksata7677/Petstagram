438b64a0ff509d6be6aef466543738a15432c3ec
Viksata <viki1204394@gmail.com>
2026-03-23 16:31:56 +0200

    Call super method second time to get id for the slug

--- petstagram/pets/models.py
+++ petstagram/pets/models.py
@@ -9,9 +9,11 @@ class Pet(models.Model):
     name = models.CharField(max_length=30)
     personal_photo = models.URLField()
     date_of_birth = models.DateField(blank=True, null=True)
-    slug = models.SlugField(null=False, blank=True, unique=True)
+    slug = models.SlugField(null=True, blank=True, unique=True)
 
     def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        
         if not self.slug:
             self.slug = slugify(f"{self.name}-{self.id}")
             
