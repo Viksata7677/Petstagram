c21a1d069c6c0c321ffb659e1db2a87d9d0276e9
Viksata <viki1204394@gmail.com>
2026-03-23 16:05:49 +0200

    Added urls for pets app

--- petstagram/pets/views.py
+++ petstagram/pets/views.py
@@ -7,13 +7,13 @@ def pet_add(request):
     return render(request, 'pets/pet-add-page.html')
 
 
-def pet_details(request):
+def pet_details(request, username: str, pet_slug: str):
     return render(request, 'pets/pet-details-page.html')
 
 
-def pet_edit(request):
+def pet_edit(request, username: str, pet_slug: str):
     return render(request, 'pets/pet-edit-page.html')
 
 
-def pet_delete(request):
+def pet_delete(request, username: str, pet_slug: str):
     return render(request, 'pets/pet-delete-page.html')
