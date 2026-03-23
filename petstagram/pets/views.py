c6301e5b4f878c4461936e77dc9253c66b617cba
Viksata <viki1204394@gmail.com>
2026-03-23 15:26:35 +0200

    Basic views for pets app

--- petstagram/pets/views.py
+++ petstagram/pets/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def pet_add(request):
+    return render(request, 'pets/pet-add-page.html')
+
+
+def pet_details(request):
+    return render(request, 'pets/pet-details-page.html')
+
+
+def pet_edit(request):
+    return render(request, 'pets/pet-edit-page.html')
+
+
+def pet_delete(request):
+    return render(request, 'pets/pet-delete-page.html')
