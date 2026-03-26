79d2942f3d515a25af2a313c09986b4116c5ef64
Viksata <viki1204394@gmail.com>
2026-03-26 20:23:56 +0200

    Added comments to the post

--- petstagram/common/models.py
+++ petstagram/common/models.py
@@ -7,6 +7,12 @@ from petstagram.photos.models import Photo
 
 
 class Comment(models.Model):
+    class Meta:
+        indexes = [
+            models.Index(fields=['date_time_of_publication'])
+        ]
+        ordering = ['-date_time_of_publication']
+
     text = models.TextField(max_length=300)
     date_time_of_publication = models.DateTimeField(auto_now_add=True)
     to_photo = models.ForeignKey(to=Photo, on_delete=models.CASCADE)
