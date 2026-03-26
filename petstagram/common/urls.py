c8458eb4e7b313de522e4dceb03f0c4037ecc543
Viksata <viki1204394@gmail.com>
2026-03-26 19:24:04 +0200

    Created share functionality

--- petstagram/common/urls.py
+++ petstagram/common/urls.py
@@ -4,5 +4,6 @@ from petstagram.common import views
 
 urlpatterns = [
     path('', views.show_all_photos, name='homepage'),
-    path('like/<int:photo_id>/', views.likes_functionality, name='like')
+    path('like/<int:photo_id>/', views.likes_functionality, name='like'),
+    path('share/<int:photo_id>/', views.share_functionality, name='share')
 ]
