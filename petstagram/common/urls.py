a3c771e25ee3f2df6c37ee34c15f5fa7786e0c63
Viksata <viki1204394@gmail.com>
2026-03-26 19:18:02 +0200

    Created like functionality

--- petstagram/common/urls.py
+++ petstagram/common/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from petstagram.common import views
 
 urlpatterns = [
-    path('', views.show_all_photos, name='homepage')
+    path('', views.show_all_photos, name='homepage'),
+    path('like/<int:photo_id>/', views.likes_functionality, name='like')
 ]
--- petstagram/common/views.py
+++ petstagram/common/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
+from petstagram.common.models import Like
 from petstagram.photos.models import Photo
 
 
@@ -12,3 +13,15 @@ def show_all_photos(request):
         'all_photos': all_photos
     }
     return render(request, 'common/home-page.html', context)
+
+
+def likes_functionality(request, photo_id: int):
+    liked_object = Like.objects.filter(to_photo_id=photo_id).first()
+
+    if liked_object:
+        liked_object.delete()
+    else:
+        like = Like(to_photo_id=photo_id)
+        like.save()
+
+    return redirect(request.META.get('HTTP_REFERER') + f'#{photo_id}')
