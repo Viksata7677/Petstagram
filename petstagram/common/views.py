9b2e3b1178d9487661896fdf5bc905efd6ecc83e
Viksata <viki1204394@gmail.com>
2026-03-26 19:03:02 +0200

    Show all posts on homepage

--- petstagram/common/views.py
+++ petstagram/common/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 
+from petstagram.photos.models import Photo
+
+
 # Create your views here.
 
-def homepage(request):
-    return render(request, 'common/home-page.html')
+def show_all_photos(request):
+    all_photos = Photo.objects.all()
+
+    context = {
+        'all_photos': all_photos
+    }
+    return render(request, 'common/home-page.html', context)
