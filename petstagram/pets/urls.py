07cd27136b6bc53892fa2c4f04cbd9399388f330
Viksata <viki1204394@gmail.com>
2026-03-27 20:57:10 +0200

    Fixed urls

--- petstagram/pets/urls.py
+++ petstagram/pets/urls.py
@@ -4,7 +4,7 @@ from petstagram.pets import views
 
 urlpatterns = [
     path('add/', views.pet_add, name='pet-add'),
-    path('<str:username>/pet/<slug:pet_slug>', include([
+    path('<str:username>/pet/<slug:pet_slug>/', include([
         path('', views.pet_details, name='pet-details'),
         path('edit/', views.pet_edit, name='pet-edit'),
         path('delete/', views.pet_delete, name='pet-delete')
