784e2bf562d83afdffbc072b7bc4e64694411d2e
Viksata <viki1204394@gmail.com>
2026-03-27 21:32:22 +0200

    Added setting for media files

--- petstagram/photos/models.py
+++ petstagram/photos/models.py
@@ -9,7 +9,7 @@ from petstagram.photos.validators import FileSizeValidator
 
 
 class Photo(models.Model):
-    photo = models.ImageField(validators=[FileSizeValidator(5)])
+    photo = models.ImageField(upload_to='', validators=[FileSizeValidator(5)])
     description = models.CharField(max_length=300, validators=[MinLengthValidator(10)], blank=True, null=True)
     location = models.CharField(max_length=30, blank=True, null=True)
     tagged_pets = models.ManyToManyField(to=Pet, blank=True)
